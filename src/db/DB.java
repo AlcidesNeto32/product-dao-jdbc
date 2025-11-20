53b716c6777522fbe2609b093a1dc7a38b6323d3
AlcidesNeto32 <alcidesnda@gmail.com>
2025-11-20 20:51:48 -0300

    data base methos and settings added

---
+++ src/db/DB.java
@@ -0,0 +1,57 @@
+package db;
+
+import java.sql.*;
+
+public class DB {
+    final static String url = "jdbc:mysql://localhost:3306/product_dao";
+    final static String user = "alcides";
+    final static String password = "Junit03.";
+
+    public static Connection connection() {
+        Connection connection = null;
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        } else {
+            throw new DBException("[ERROR] an error unexpected happened!");
+        }
+    }
+
+    public static void closePreparedStatements(PreparedStatement preparedStatement) {
+        if (preparedStatement != null) {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        } else {
+            throw new DBException("[ERROR] an error unexpected happened!");
+        }
+    }
+
+    public static void closeResultSet(ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        } else {
+            throw new DBException("[ERROR] an error unexpected happened!");
+        }
+    }
+}
