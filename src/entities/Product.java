c5ff9256e0f5e4e0be2b7235fde903950e89a375
AlcidesNeto32 <alcidesnda@gmail.com>
2025-11-20 21:09:36 -0300

    package entities added and entity Product created

---
+++ src/entities/Product.java
@@ -0,0 +1,85 @@
+package entities;
+
+import java.util.Objects;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
+    private String department;
+
+    public Product(int id, String name, double price, int quantity, String department) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.department = department;
+    }
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
